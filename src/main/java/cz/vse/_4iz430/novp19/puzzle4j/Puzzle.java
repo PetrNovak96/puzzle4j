f84548df7c9208c5e51703ccd84645842c917737
Petr Novák <novp19@vse.cz>
2019-12-26 16:31:14 +0100

    rename puzzle

--- src/main/java/cz/vse/_4iz430/novp19/puzzle4j/Puzzle.java
+++ src/main/java/cz/vse/_4iz430/novp19/puzzle4j/Puzzle.java
@@ -4,14 +4,14 @@ package cz.vse._4iz430.novp19.puzzle4j;
 import java.util.List;
 import java.util.Set;
 
-public abstract class Riddle<T extends Situation> {
+public abstract class Puzzle<T extends Situation> {
 
     private T initialSituation;
     private Set<Move> moves;
     private Set<Rule> rules;
     private Solver<T> solver;
 
-    public Riddle() {
+    public Puzzle() {
         this.solver = new Solver<T>();
         this.initialSituation = initialSituation();
         this.rules = this.rules();
